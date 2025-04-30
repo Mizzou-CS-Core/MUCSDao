431c67cf2e38fe1fb32b0ac626ac795baa30b665
Matt Marlow <thequantumblink@gmail.com>
2025-04-30 14:23:40 -0500

    fix: added init

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name="mucs_database",
-  version="0.1.0",
+  version="0.2.0",
   packages=find_packages(include=["database", "database.*"]),
   install_requires=[
   "canvas_lms_api @ git+https://github.com/Mizzou-CS-Core/CanvasRequestLibrary.git#egg=canvas_lms_api",
